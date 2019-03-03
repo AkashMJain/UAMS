e455919e9177b5bea2c8d46c25f38beccaeb6635
akash <akashjaincoder@icloud.com>
2019-03-03 13:39:59 +0530

    script added

---
+++ run.sh
@@ -0,0 +1,15 @@
+
+
+proto=$PWD'/caffe/deploy.prototxt.txt'
+echo $proto
+
+weights=$PWD'/caffe/res10_300x300_ssd_iter_140000.caffemodel'
+echo $weights
+
+video=$PWD'/test_data/test_data2.mov'
+echo $video
+
+python UAMS.py -p $proto -w $weights -v $video
+
+
+# python UAMS.py -p caffe/deploy.prototxt.txt -w caffe/res10_300x300_ssd_iter_140000.caffemodel -v test_data/test_data2.mov
